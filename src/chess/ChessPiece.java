167ed4e32faba637e7dc25f2795b29eca29ba059
Victor Rossetto <victor_rossetto1@hotmail.com>
2021-07-15 15:14:50 -0300

    Logica de check

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -6,14 +6,18 @@ import boardgame.Position;
 
 public abstract class ChessPiece extends Piece{
 	private Color color;
+	
 
 	public ChessPiece(Board board, Color color) {
 		super(board);
 		this.color = color;
 	}
-
+	
 	public Color getColor() {
 		return color;
+	}
+	public ChessPosition getChessPosition() {
+		return ChessPosition.fromPosition(position);
 	}
 	protected boolean  isThereOpponentPiece(Position position) {
 		ChessPiece p=(ChessPiece)getBoard().piece(position);
